6cf675f9b609ed59c3741e4da03ad8caae0ef7ef
juanortega <dannyortega0627@gmail.com>
2025-10-09 21:29:53 -0500

    Splash screen para app

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomePage(),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 238, 238, 240), // color de fondo
+      body: Center(
+        child: Text(
+          "🎉 Bienvenido al Carnaval 🎉",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            color: const Color.fromARGB(255, 10, 10, 10),
+          ),
+        ),
+      ),
+    );
+  }
+}
